315a1c94ad3032dd8a3565392b1826fd7b8ef6d9
afzal <afzal@afzal-PC.mshome.net>
2020-09-28 12:38:56 +0530

    Added antMatchers for /admin/ and /user/ also the configure method with WebSecurity implementation

--- coronakit-sba2-springboot-mvc-master/coronakit-sba2-springboot-mvc-master/src/main/java/com/eval/coronakit/config/SecurityConfig.java
+++ coronakit-sba2-springboot-mvc-master/coronakit-sba2-springboot-mvc-master/src/main/java/com/eval/coronakit/config/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.eval.coronakit.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
@@ -34,8 +36,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.csrf().disable();
 		http.headers().frameOptions().disable();
 		
+		http.authorizeRequests()
+		.antMatchers("/admin/**").hasRole("ADMIN")
+		.antMatchers("/user/**").hasRole("USER");
+
+		http.formLogin().loginPage("/validate").failureUrl("/custom-login?error=true").defaultSuccessUrl("/home")
+		.usernameParameter("username").passwordParameter("password");
+
+		http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/");
+
+		http.exceptionHandling().accessDeniedPage("/views/accessDeniedPage.jsp");
+		
 		// Add Your Application Based Security config here
 		
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
 	}
 }
 
